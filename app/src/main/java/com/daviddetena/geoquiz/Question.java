04dd61236ece93f3d0c4602a03471405c84ed44f
David de Tena <daviddetenacalvo@gmail.com>
2015-09-22 00:34:29 +0200

    MVC added. New button for Next question added

---
+++ app/src/main/java/com/daviddetena/geoquiz/Question.java
@@ -0,0 +1,31 @@
+package com.daviddetena.geoquiz;
+
+/**
+ * Created by daviddetena on 21/09/15.
+ */
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+}
--- app/src/main/java/com/daviddetena/geoquiz/QuizActivity.java
+++ app/src/main/java/com/daviddetena/geoquiz/QuizActivity.java
@@ -6,21 +6,40 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
+    private TextView mQuestionTextView;
     private Button mFalseButton;
     private Button mTrueButton;
+    private Button mNextButton;
+    private int mCurrentIndex = 0;      // current index of question in Array
+
+    // Initial array of questions
+    private Question[] mQuestionBank = new Question[]{
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, true),
+            new Question(R.string.question_africa, true),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        // Wire up buttons
+        // Wire up View
+        // Sets up the TextView with the question title of the current index in the array
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        updateQuestion();
+
+        // Wire up Buttons
         mFalseButton = (Button) findViewById(R.id.false_button);
         mTrueButton = (Button) findViewById(R.id.true_button);
+        mNextButton = (Button) findViewById(R.id.next_button);
 
         // Set up listener for the buttons and make a toast appear informing about the answer
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -34,6 +53,16 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Set up New Question Title when clicking Next
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Next index and update text of the new question
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
             }
         });
     }
@@ -58,5 +87,12 @@ public class QuizActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+     // UTILS
+    private void updateQuestion(){
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
     }
 }
